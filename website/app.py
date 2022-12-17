d19f88c4e0bd92362b021052aa7ac800f4c668d3
devabdth <a.tharwat@cubersio.com>
2022-12-17 14:09:39 +0200

    Fix Template & Static Directories

--- website/app.py
+++ website/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
+
 from config import Config
 from setup import Setup
 
 config: Config = Config()
-app: Flask = Flask("STORIESCLUB_WEBSITE")
+app: Flask = Flask(
+    "STORIESCLUB_WEBSITE", 
+    template_folder=os.path.join(os.path.dirname(__file__), "./templates"),
+    static_folder=os.path.join(os.path.dirname(__file__), "./static"),
+)
 setup: Setup = Setup(app= app, config=config)
 
 
