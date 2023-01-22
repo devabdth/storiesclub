7938318f2eca0ad3bf893d3289929f50b3e0695b
devabdth <a.tharwat@cubersio.com>
2023-01-22 09:18:18 +0200

    Social Media Icons Added

--- ComingSoon/app.py
+++ ComingSoon/app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
+import os
 
+app: Flask= Flask(
+	"STORIESCLUB_COMING_SOON",
+	template_folder=os.path.join(os.path.dirname(__file__), "./templates"),
+	static_folder=os.path.join(os.path.dirname(__file__), "./static"),
+)
 
-app: Flask= Flask("STORIESCLUB_COMING_SOON")
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template(
-        "index.html"
-    )
+	return render_template(
+		"index.html"
+	)
 
 app.run(
-    debug= True,
-    port= 3000
+	debug= True,
+	port= 3000
 )
+	
