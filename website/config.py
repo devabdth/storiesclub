1a40fa5c93d945a873d70433afbdfac3c632cb85
devabdth <a.tharwat@cubersio.com>
2023-01-28 09:41:43 +0200

    Update Read env method

--- website/config.py
+++ website/config.py
@@ -18,32 +18,25 @@ class Config():
         self.debug_mode_token = "DEBUG"
         self.production_mode_token = "PROD"
 
-        self.port = os.environ.get("PORT") or 3030
-        self.mode = os.environ.get("MODE") or self.debug_mode_token
-        self.auth_key = os.environ.get("AUTH_KEY") or "1234567890"
-        self.url = os.environ.get("URL") or "http://127.0.0.1:5000"
-        self.db_url= "mongodb://localhost/storiesclub"
-        #self.db_url = os.environ.get(
-            #"DB_URL") or "mongodb+srv://website:cqoS01gP7RzULu9v@storiesclub.twg2b.mongodb.net/storiesclub?retryWrites=true&w=majority"
+        self.port = os.environ["PORT"] if "PORT" in os.environ else 3030
+        self.mode = os.environ["MODE"] if "MODE" in os.environ else self.debug_mode_token
+        self.auth_key = os.environ["AUTH_KEY"] if "AUTH_KEY" in os.environ else "1234567890"
+        self.url = os.environ["URL"] if "URL" in os.environ else "http://127.0.0.1:5000"
+        self.db_url = os.environ["DB_URL"] if "DB_URL" in os.environ else "mongodb+srv://website:cqoS01gP7RzULu9v@storiesclub.twg2b.mongodb.net/storiesclub?retryWrites=true&w=majority"
 
-        self.email_model_email = os.environ.get(
-            "EMAIL_MODEL_EMAIL") or "no.reply@storiesclub.net"
-        self.email_model_access_key = os.environ.get(
-            "EMAIL_MODEL_ACCESS_KEY") or "2892022-CUBERS-MAILING-AUTH-KEY@StroiesClub.Password@Cubers IO Inc."
+        self.email_model_email = os.environ["EMAIL_MODEL_EMAIL"] if "EMAIL_MODEL_EMAIL" in os.environ else "no.reply@storiesclub.net"
+        self.email_model_access_key = os.environ["EMAIL_MODEL_ACCESS_KEY"] if "EMAIL_MODEL_ACCESS_KEY" in os.environ else "2892022-CUBERS-MAILING-AUTH-KEY@StroiesClub.Password@Cubers IO Inc."
         # TODO
-        self.header_desc = os.environ.get("HEADER_DESC") or """"""
+        self.header_desc = os.environ["HEADER_DESC"] if "HEADER_DESC" in os.environ else """"""
         # TODO
-        self.header_keywords = os.environ.get("HEADER_KEYWORDS") or ""
+        self.header_keywords = os.environ["HEADER_KEYWORDS"] if "HEADER_KEYWORDS" in os.environ else ""
 
-        self.facebook = os.environ.get(
-            "FACEBOOK") or "https://facebook.com/storiesclub.net"
-        self.linkedin = os.environ.get(
-            "LINKEDIN") or "https://linkedin.com/company/storiesclun.net"
-        self.instagram = os.environ.get(
-            "INSTAGTAM") or "https://instagram.com/storiesclun.net"
-        self.email = os.environ.get("CONTACT_EMAIL") or "info@cubersio.com"
-        self.phone = os.environ.get("CONTACT_PHONE") or "+20 112 916 4522"
-        self.address = os.environ.get("CONTACT_ADDRESS") or ""
+        self.facebook = os.environ["FACEBOOK"] if "FACEBOOK" in os.environ else "https://facebook.com/storiesclub.net"
+        self.linkedin = os.environ["LINKEDIN"] if "LINKEDIN" in os.environ else "https://linkedin.com/company/storiesclun.net"
+        self.instagram = os.environ["INSTAGTAM"] if "INSTAGTAM" in os.environ else "https://instagram.com/storiesclun.net"
+        self.email = os.environ["CONTACT_EMAIL"] if "CONTACT_EMAIL" in os.environ else "info@cubersio.com"
+        self.phone = os.environ["CONTACT_PHONE"] if "CONTACT_PHONE" in os.environ else "+20 112 916 4522"
+        self.address = os.environ["CONTACT_ADDRESS"] if "CONTACT_ADDRESS" in os.environ else ""
 
         self.website_content = WebsiteContent()
         self.categories = CategoriesContent()
